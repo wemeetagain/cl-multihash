22397b30135425aa4bb17c3c414b5444d7c68377
Cayman Nava <caymannava@gmail.com>
2015-11-27 15:40:18 -0800

    remove docs in favor of README docs

--- docs/manifest.lisp
+++
@@ -1,7 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (:cl-multihash)
- :documents ((:title "CL-Multihash"
-              :authors ("Cayman Nava")
-              :output-format (:type :single-html
-                              :template :gamma)
-              :sources ("manual.scr"))))
